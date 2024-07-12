afb9697944a20dd505c74cc4f44ddb55bb913b3f
Evan Runyon <136279901+e-runyon@users.noreply.github.com>
2024-07-11 22:38:35 -0500

    Update app.js
    Add health check endpoint

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.get("/health", (req, res) => {
+  res.sendStatus(200);
+});
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
@@ -13,7 +17,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title>Hello Render</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
